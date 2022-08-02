b06432d25c47ca72612d7899dd3cf782564cf3ab
Hernandez Maikol <maikol_hernandez_rm@hotmail.com>
2022-08-02 19:41:16 -0400

    Components - Slide - adding some media rules

--- src/components/Slide.jsx
+++ src/components/Slide.jsx
@@ -2,11 +2,11 @@ export default function Slide(props) {
   // local state
   const buttons = [1, 2, 3, 4]
   return (
-    <section className="flex-1 px-10">
-      <div className="rounded-xl overflow-hidden">
+    <section className="flex-1 lg:px-10">
+      <div className="lg:rounded-xl overflow-hidden">
         <img className="block w-full" src="/image-product-1.jpg" alt="" />
       </div>
-      <div className="flex gap-8 mt-8">
+      <div className="gap-8 mt-8 hidden lg:flex">
         {buttons.map((button, index) => (
           <button key={index} className="flex-1 border-2 border-transparent rounded-xl overflow-hidden">
             <img className="w-full h-full object-cover" src={`/image-product-${index + 1}-thumbnail.jpg`} alt="product image" />
