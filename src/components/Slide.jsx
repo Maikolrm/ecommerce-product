69366f8085ef4b35568d11e01947b928ec104905
Hernandez Maikol <maikol_hernandez_rm@hotmail.com>
2022-08-02 17:35:51 -0400

    Components - Slide - creating component basic structure

--- src/components/Slide.jsx
+++ src/components/Slide.jsx
@@ -1,7 +1,18 @@
 export default function Slide(props) {
+  // local state
+  const buttons = [1, 2, 3, 4]
   return (
-    <section>
-      Product Slide
+    <section className="flex-1 px-10">
+      <div className="rounded-xl overflow-hidden">
+        <img className="block w-full" src="/image-product-1.jpg" alt="" />
+      </div>
+      <div className="flex gap-8 mt-8">
+        {buttons.map((button, index) => (
+          <button key={index} className="flex-1 border-2 border-transparent rounded-xl overflow-hidden">
+            <img className="w-full h-full object-cover" src={`/image-product-${index + 1}-thumbnail.jpg`} alt="product image" />
+          </button>
+        ))}
+      </div>
     </section>
   )
 }
