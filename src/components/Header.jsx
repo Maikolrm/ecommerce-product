497351abfd6b00aefbedea28846d17d576c463c7
Hernandez Maikol <maikol_hernandez_rm@hotmail.com>
2022-08-02 19:52:56 -0400

    Components - Header - adding responsive menu btn

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,9 @@
 export default function Header(props) {
   return (
     <header className="flex items-center p-4 border-b border-gray-200">
+      <button className="w-10 h-10 mr-2 bg-light-blue rounded-md text-base text-vark-blue leading-10 lg:hidden">
+        <i className="fa-solid fa-bars"></i>
+      </button>
       <a href="#">
         <img src="/logo.svg" alt="sneakers" />
       </a>
