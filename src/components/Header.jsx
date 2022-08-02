8105280c0e4d09a84486df7ab4788672a524f391
Hernandez Maikol <maikol_hernandez_rm@hotmail.com>
2022-08-02 19:40:35 -0400

    Components - Header - adding some media query rules

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,10 @@
 export default function Header(props) {
   return (
-    <header className="flex items-center border-b border-gray-200">
+    <header className="flex items-center p-4 border-b border-gray-200">
       <a href="#">
         <img src="/logo.svg" alt="sneakers" />
       </a>
-      <nav className="flex gap-4 pl-20">
+      <nav className="flex gap-4 pl-20 hidden">
         <a href="#" className="inline-block py-12 border-b-4 border-transparent text-dark-blue hover:border-pr-orange hover:text-vark-blue">Collections</a>
         <a href="#" className="inline-block py-12 border-b-4 border-transparent text-dark-blue hover:border-pr-orange hover:text-vark-blue">Men</a>
         <a href="#" className="inline-block py-12 border-b-4 border-transparent text-dark-blue hover:border-pr-orange hover:text-vark-blue">Women</a>
@@ -12,10 +12,10 @@ export default function Header(props) {
         <a href="#" className="inline-block py-12 border-b-4 border-transparent text-dark-blue hover:border-pr-orange hover:text-vark-blue">Contact</a>
       </nav>
       <div className="flex items-center ml-auto">
-        <button className="w-10 h-10 mr-10 text-xl text-center text-dark-blue leading-10 hover:text-vark-blue">
+        <button className="w-10 h-10 mr-4 text-base lg:text-xl text-center text-dark-blue leading-10 hover:text-vark-blue">
           <i className="fa-solid fa-shopping-cart"></i>
         </button>
-        <button className="inline-block w-14 h-14 border-2 border-transparent rounded-full outline-none overflow-hidden hover:border-pr-orange focus:border-pr-orange">
+        <button className="inline-block w-10 h-10 lg:w-14 lg:h-14 border-2 border-transparent rounded-full outline-none overflow-hidden hover:border-pr-orange focus:border-pr-orange">
           <img src="/image-avatar.png" alt="hernandez maikol" />
         </button>
       </div>
