d7e18f78e623b8799ee4a067eb6836a40c27fd21
Hernandez Maikol <maikol_hernandez_rm@hotmail.com>
2022-08-02 16:29:02 -0400

    Components - Header - adding component basic structure

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,22 @@
 export default function Header(props) {
   return (
-    <header>
-      Document header
+    <header className="flex items-center py-8 border-b border-gray-200">
+      <a href="#">
+        <img src="/logo.svg" alt="sneakers" />
+      </a>
+      <nav>
+        <a href="#">Collections</a>
+        <a href="#">Men</a>
+        <a href="#">Women</a>
+        <a href="#">About</a>
+        <a href="#">Contact</a>
+      </nav>
+      <div className="flex ml-auto">
+        <button>c</button>
+        <button className="inline-block w-16 h-16 rounded-full overflow-hidden">
+          <img src="/image-avatar.png" alt="hernandez maikol" />
+        </button>
+      </div>
     </header>
   )
 }
