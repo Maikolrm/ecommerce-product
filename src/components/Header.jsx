e6569aada427348d21b4ddf94b8e6a1df8ad77a8
Hernandez Maikol <maikol_hernandez_rm@hotmail.com>
2022-08-02 17:10:36 -0400

    Components - Header - adding some basic styles to header elements

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,21 @@
 export default function Header(props) {
   return (
-    <header className="flex items-center py-8 border-b border-gray-200">
+    <header className="flex items-center border-b border-gray-200">
       <a href="#">
         <img src="/logo.svg" alt="sneakers" />
       </a>
-      <nav>
-        <a href="#">Collections</a>
-        <a href="#">Men</a>
-        <a href="#">Women</a>
-        <a href="#">About</a>
-        <a href="#">Contact</a>
+      <nav className="flex gap-4 pl-20">
+        <a href="#" className="inline-block py-12 border-b-4 border-transparent text-dark-blue hover:border-pr-orange hover:text-vark-blue">Collections</a>
+        <a href="#" className="inline-block py-12 border-b-4 border-transparent text-dark-blue hover:border-pr-orange hover:text-vark-blue">Men</a>
+        <a href="#" className="inline-block py-12 border-b-4 border-transparent text-dark-blue hover:border-pr-orange hover:text-vark-blue">Women</a>
+        <a href="#" className="inline-block py-12 border-b-4 border-transparent text-dark-blue hover:border-pr-orange hover:text-vark-blue">About</a>
+        <a href="#" className="inline-block py-12 border-b-4 border-transparent text-dark-blue hover:border-pr-orange hover:text-vark-blue">Contact</a>
       </nav>
-      <div className="flex ml-auto">
-        <button>c</button>
-        <button className="inline-block w-16 h-16 rounded-full overflow-hidden">
+      <div className="flex items-center ml-auto">
+        <button className="w-10 h-10 mr-10 text-xl text-center text-dark-blue leading-10 hover:text-vark-blue">
+          <i className="fa-solid fa-shopping-cart"></i>
+        </button>
+        <button className="inline-block w-14 h-14 border-2 border-transparent rounded-full outline-none overflow-hidden hover:border-pr-orange focus:border-pr-orange">
           <img src="/image-avatar.png" alt="hernandez maikol" />
         </button>
       </div>
