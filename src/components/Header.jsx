d53b87d46ad6cb155be47dd91cfec1e03b975a51
Hernandez Maikol <maikol_hernandez_rm@hotmail.com>
2022-08-02 22:21:57 -0400

    Components - Header - importing and using app state and dispatch

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,22 @@
+import { useContext } from "react"
+
+// context
+import AppState from "../AppState"
+import AppDispatch from "../AppDispatch"
+
 // components
 import ShoppingCart from "./ShoppingCart"
 
 export default function Header(props) {
+  // app dispatch
+  const { cart } = useContext(AppState)
+
+  // app dispatch
+  const appDispatch = useContext(AppDispatch)
+
   return (
     <header className="relative flex items-center p-4 border-b border-gray-200">
-      <ShoppingCart />
+      <ShoppingCart visible={cart.visible} dispatch={appDispatch} />
       <button className="w-10 h-10 mr-2 bg-light-blue rounded-md text-base text-vark-blue leading-10 lg:hidden">
         <i className="fa-solid fa-bars"></i>
       </button>
@@ -19,7 +31,7 @@ export default function Header(props) {
         <a href="#" className="inline-block py-12 border-b-4 border-transparent text-dark-blue hover:border-pr-orange hover:text-vark-blue">Contact</a>
       </nav>
       <div className="flex items-center ml-auto">
-        <button className="w-10 h-10 mr-4 text-base lg:text-xl text-center text-dark-blue leading-10 hover:text-vark-blue">
+        <button onClick={() => appDispatch({ type: "show-cart", value: !cart.visible })} className="w-10 h-10 mr-4 text-base lg:text-xl text-center text-dark-blue leading-10 hover:text-vark-blue">
           <i className="fa-solid fa-shopping-cart"></i>
         </button>
         <button className="inline-block w-10 h-10 lg:w-14 lg:h-14 border-2 border-transparent rounded-full outline-none overflow-hidden hover:border-pr-orange focus:border-pr-orange">
