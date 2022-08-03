f39d59a1325c96eb5a8ab4ceab785e24b418990b
Hernandez Maikol <maikol_hernandez_rm@hotmail.com>
2022-08-02 21:30:10 -0400

    Components - ProductInfo - creating component basic structure

--- src/components/ProductInfo.jsx
+++ src/components/ProductInfo.jsx
@@ -1,4 +1,4 @@
-export default function ShoppingCart(props) {
+export default function ProductInfo(props) {
   return (
     <section className="flex-1 flex flex-col justify-center p-6 lg:px-10">
       <h2 className="font-semibold text-sm text-pr-orange uppercase">sneaker company</h2>
@@ -6,21 +6,23 @@ export default function ShoppingCart(props) {
       <p className="mt-10 text-base text-gr-blue leading-6">Adipisicing eius veritatis repellendus mollitia in Sunt beatae consectetur facilis tempora et impedit alias Pariatur eveniet ullam vero ipsum et.</p>
       <div className="mt-8">
         <div className="flex items-center justify-between">
-          <h2 className="font-bold leading-none">$125.00 <span className="text-pr-pale-oraneg">50%</span></h2>
-          <h4>250.00</h4>
+          <h2 className="flex items-center font-bold text-2xl leading-none">
+            <span>$125.00</span> <span className="inline-block ml-4 p-1 bg-pr-pale-orange rounded-sm text-base text-pr-orange leading-none">50%</span>
+          </h2>
+          <h4 className="font-bold text-sm text-gr-blue line-through">250.00</h4>
         </div>
         <form className="mt-10 lg:flex">
           <div className="flex bg-light-blue rounded-md overflow-hidden">
             <button className="w-12 h-12 text-center text-pr-orange leading-10">
               <i className="fa-solid fa-minus"></i>
             </button>
-            <input className="flex-1 text-center bg-transparent" type="number" name="count" id="count" value="0" />
+            <input className="flex-1 font-bold text-xl text-center text-vart-blue bg-transparent" type="number" name="count" id="count" value="0" />
             <button className="w-12 h-12 text-center text-pr-orange leading-10">
               <i className="fa-solid fa-plus"></i>
             </button>
           </div>
-          <button className="block w-full mt-4 lg:flex-1 lg:mt-0 lg:ml-4 font-semibold text-white text-center rounded-md bg-pr-orange shadow-xl leading-[50px]">
-           <i className="fa-solid fa-shopping-cart"></i> Add to cart
+          <button className="block w-full mt-4 font-semibold text-base text-white text-center rounded-md bg-pr-orange shadow-xl leading-[50px] hover:opacity-70 lg:flex-1 lg:mt-0 lg:ml-4">
+            <i className="inline-block mr-2 fa-solid fa-shopping-cart"></i> Add to cart
           </button>
         </form>
       </div>
