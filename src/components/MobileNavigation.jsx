79328a3953dcd12f2c206e22ec8f6a6a36af3d34
Hernandez Maikol <maikol_hernandez_rm@hotmail.com>
2022-08-04 14:36:51 -0400

    Components - MobileNavigation - creating component

---
+++ src/components/MobileNavigation.jsx
@@ -0,0 +1,20 @@
+export default function MobileNavigation(props) {
+  return (
+    <div className="absolute inset-0 bg-black/70 lg:hidden">
+      <div className="w-2/3 h-full p-6 pt-4 bg-white border-l border-light-blue sm:max-w-sm">
+        <div>
+          <button onClick={() => props.dispatch({ type: "show-mobile-nav", value: false })} className="w-10 h-10 text-xl text-left text-dark-blue leading-10">
+            <i className="fa-solid fa-times"></i>
+          </button>
+        </div>
+        <nav className="mt-10 font-bold text-lg text-vark-blue">
+          <a href="#" className="block my-4 text-vark-blue">Collectons</a>
+          <a href="#" className="block my-4 text-vark-blue">Men</a>
+          <a href="#" className="block my-4 text-vark-blue">Women</a>
+          <a href="#" className="block my-4 text-vark-blue">About</a>
+          <a href="#" className="block my-4 text-vark-blue">Contact</a>
+        </nav>
+      </div>
+    </div>
+  )
+}
