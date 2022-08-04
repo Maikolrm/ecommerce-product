fcdb0f6050473b4bf7c792ccc1fa19f5bc46cd3e
Hernandez Maikol <maikol_hernandez_rm@hotmail.com>
2022-08-04 13:39:29 -0400

    Components - Header - adding shopping cart items count

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -31,10 +31,19 @@ export default function Header(props) {
         <a href="#" className="inline-block py-12 border-b-4 border-transparent text-dark-blue hover:border-pr-orange hover:text-vark-blue">Contact</a>
       </nav>
       <div className="flex items-center ml-auto">
-        <button onClick={() => appDispatch({ type: "show-cart", value: !cart.visible })} className="w-10 h-10 mr-4 text-base lg:text-xl text-center text-dark-blue leading-10 hover:text-vark-blue">
-          <i className="fa-solid fa-shopping-cart"></i>
-        </button>
-        <button className="inline-block w-10 h-10 lg:w-14 lg:h-14 border-2 border-transparent rounded-full outline-none overflow-hidden hover:border-pr-orange focus:border-pr-orange">
+        <div className="relative">
+          {Boolean(cart.items.length) && (
+            <span className="inline-block absolute -top-1 right-0 w-4 bg-pr-orange rounded-md font-semibold text-xs text-center text-white leading-none">
+              {cart.items.length}
+            </span>
+          )}
+          <button
+            onClick={() => appDispatch({ type: "show-cart", value: !cart.visible })}
+            className="w-10 h-10 text-base lg:text-xl text-center text-dark-blue leading-10 hover:text-vark-blue">
+            <i className="fa-solid fa-shopping-cart"></i>
+          </button>
+        </div>
+        <button className="block w-10 h-10 ml-4 lg:w-14 lg:h-14 border-2 border-transparent rounded-full outline-none overflow-hidden hover:border-pr-orange focus:border-pr-orange">
           <img src="/image-avatar.png" alt="hernandez maikol" />
         </button>
       </div>
