de6dd800861d8bc4025ecb5d3356279e742dfadf
Hernandez Maikol <maikol_hernandez_rm@hotmail.com>
2022-08-08 12:33:41 -0400

    Components - SlideControls - dispatching event ( select product imgae )

--- src/components/SlideControls.jsx
+++ src/components/SlideControls.jsx
@@ -1,8 +1,15 @@
+import { useContext } from "react"
+
+// context
+import AppDispatch from "../AppDispatch"
+
 export default function SlideControls(props) {
+  // app dispatch
+  const appDispatch = useContext(AppDispatch)
   return (
     <div className={props.styles}>
       {props.product.images.map((button, index) => (
-        <button key={index} className="flex-1 border-2 border-transparent rounded-xl overflow-hidden">
+        <button onClick={() => appDispatch({ type: "select-product-image", value: index })} key={index} className="flex-1 border-2 border-transparent rounded-xl overflow-hidden">
           <img
             className="w-full h-full object-cover"
             src={`/image-product-${index + 1}-thumbnail.jpg`}
