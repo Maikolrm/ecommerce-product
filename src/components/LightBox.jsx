4f74e281deaf7ff86ef39caf49c7125f177f37c6
Hernandez Maikol <maikol_hernandez_rm@hotmail.com>
2022-08-08 12:59:06 -0400

    Components - Lightbox - importging and rendering components ( SlideImage & SlideControls )

--- src/components/LightBox.jsx
+++ src/components/LightBox.jsx
@@ -1,17 +1,21 @@
 // components
-import ProductSlide from "./ProductSlide"
+import SlideImage from "./SlideImage"
+import SlideControls from "./SlideControls"
 
 export default function Lightbox(props) {
   return (
     <div className="absolute z-10 inset-0 flex items-center justify-center px-10 bg-black/80">
-      <div className="max-w-md">
+      <div className="max-w-lg">
         <div className="flex">
           <button className="w-10 h-10 ml-auto text-xl text-white leading-10">
             <i className="fa-solid fa-times"></i>
           </button>
         </div>
         <div className="mt-2 rounded-xl overflow-hidden">
-          <ProductSlide product={props.product} selectedImage={props.selectedImage} dispatch={props.dispatch} />
+          <SlideImage product={props.product} styles="" dispatch={props.dispatch} />
+        </div>
+        <div>
+          <SlideControls product={props.product} styles="flex gap-6 pt-6" dispatch={props.dipatch} />
         </div>
       </div>
     </div>
