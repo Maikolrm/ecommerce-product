a385e3b677f14bb96cf54823bdf98e4cff9a2011
Hernandez Maikol <maikol_hernandez_rm@hotmail.com>
2022-08-08 13:12:24 -0400

    Components - App - improving media rules on main container

--- src/App.jsx
+++ src/App.jsx
@@ -79,7 +79,7 @@ function App() {
         <div className="relative container min-h-screen m-auto">
           {state.lightBox && <LightBox product={state.product} selectedImage={state.selectedImage} dispatch={state.dispatch} />}
           <Header dispatch={dispatch} />
-          <div className="lg:flex lg:py-16">
+          <div className="xl:flex lg:py-16">
             <ProductSlide product={state.product} selectedImage={state.selectedImage} dispatch={dispatch} />
             <ProductInfo product={state.product} dispatch={dispatch} />
           </div>
