716671b7a400a0dbaac457e452cc4fb3b3db22b2
Hernandez Maikol <maikol_hernandez_rm@hotmail.com>
2022-08-09 14:09:28 -0400

    Components - Lightbox - importing and rendering ImageContorls component

--- src/components/LightBox.jsx
+++ src/components/LightBox.jsx
@@ -1,22 +1,22 @@
 // components
 import SlideImage from "./SlideImage"
 import SlideControls from "./SlideControls"
+import ImageControls from "./ImageControls"
 
 export default function Lightbox(props) {
   return (
-    <div className="absolute z-10 inset-0 flex items-center justify-center px-10 bg-black/80">
+    <div className="absolute z-[100] inset-0 flex items-center justify-center px-10 bg-black/80">
       <div className="max-w-lg">
         <div className="flex">
           <button onClick={() => props.dispatch({ type: "show-lightbox", value: false })} className="w-10 h-10 ml-auto text-2xl text-white text-right leading-10">
             <i className="fa-solid fa-times"></i>
           </button>
         </div>
-        <div className="mt-2 rounded-xl overflow-hidden">
-          <SlideImage product={props.product} styles="" dispatch={props.dispatch} />
+        <div className="relative mt-2">
+          <ImageControls styles="w-[109%] left-[-4.5%]" dispatch={props.dispatch} />
+          <SlideImage product={props.product} styles="rounded-xl overflow-hidden" dispatch={props.dispatch} />
         </div>
-        <div>
-          <SlideControls product={props.product} styles="flex gap-6 pt-6 md:px-6" dispatch={props.dipatch} />
-        </div>
+        <SlideControls product={props.product} styles="flex gap-6 pt-6 md:px-6" dispatch={props.dipatch} />
       </div>
     </div>
   )
